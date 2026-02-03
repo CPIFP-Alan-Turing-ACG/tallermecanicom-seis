95d9f29b2b976400377d470df09dc97f3304c0bc
Sergio <sortalm@g.educaand.es>
2026-02-03 12:53:45 +0100

    push Inventario.java

--- src/Inventario.java
+++ src/Inventario.java
@@ -1,6 +1,6 @@
 public class Inventario {
     public void comprobarStock() {
         // TODO: Implementar lógica (Alumno C)
-        System.out.println("Función no implementada aún");
+        System.out.println("50 bisis, 15 xuxes, 80 sapos");
     }
 }
